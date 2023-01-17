654d9b90a9318d64aa0ec0e2b79bb6a0103ca62c
michaelcabiareyes <69967827+michaelcabiareyes@users.noreply.github.com>
2023-01-17 15:53:40 +0800

    feature: Add main.py for the library.

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import json
+import argparse
+
+def json_to_csv(input_json_filepath, output_csv_filename):
+	data = json.load(open(input_json_filepath))
+	data = pd.json_normalize(data)
+	data.to_csv(output_csv_filename)
+	return
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("input_file", type=str, help="JSON Filepath or Data to be parsed.")
+	parser.add_argument("output_file", type=str, help="The filename of the output CSV file.")
+	args = parser.parse_args()
+
+	json_to_csv(args.input_file, args.output_file)
+	return
+
+
+if __name__ == "__main__":
+	main()
+
